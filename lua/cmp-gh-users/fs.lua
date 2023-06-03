e01e08dc71fee7ce351756168f029e389d514e0d
Fred Dubois <169247+duboisf@users.noreply.github.com>
2023-06-03 08:59:48 -0400

    chore: add type annotations to fs.lua

--- lua/cmp-gh-users/fs.lua
+++ lua/cmp-gh-users/fs.lua
@@ -18,21 +18,25 @@ function fs.read_file(path)
   log("read_file path=" .. path, vim.log.levels.DEBUG)
   local err
   local fd
+  ---@type nil|string, integer|nil
   err, fd = a.uv.fs_open(path, "r", 438)
   if err or fd == nil then
     return "failed to open file: " .. tostring(err), nil
   end
   local stat
+  ---@type nil|string, {size: integer}|nil
   err, stat = a.uv.fs_stat(path)
   if err or stat == nil then
     return "failed to stat file: " .. tostring(err), nil
   end
   local data
+  ---@type nil|string, string|nil
   err, data = a.uv.fs_read(fd, stat.size)
   if err or data == nil then
     return "failed to read file: " .. tostring(err), nil
   end
   local success
+  ---@type nil|string, boolean|nil
   err, success = a.uv.fs_close(fd)
   if err or not success then
     return "failed to close file: " .. tostring(err), nil
@@ -47,11 +51,13 @@ end
 ---@param data string
 ---@return nil|string err
 function fs.write_file(path, data)
+  ---@type nil|string, integer|nil
   local err, fd = a.uv.fs_open(path, "w+", 438)
   if err or fd == nil then
     return "failed to open file: " .. tostring(err)
   end
   local bytes
+  ---@type nil|string, integer|nil
   err, bytes = a.uv.fs_write(fd, data, 0)
   if err or bytes == nil then
     return "failed to write file: " .. tostring(err)
@@ -63,6 +69,7 @@ end
 ---@param path string
 ---@return nil|string err, boolean exists
 function fs.dir_exists(path)
+  ---@type nil|string, {type: string}|nil
   local err, stat = a.uv.fs_stat(path)
   if err then
     -- ENOENT: no such file or directory
@@ -70,6 +77,9 @@ function fs.dir_exists(path)
       return nil, false
     end
     return err, false
+  end
+  if stat == nil then
+    return "path '" .. path .. "' exists but stat returned nil", false
   end
   if stat.type ~= "directory" then
     return "path '" .. path .. "' exists but is a " .. stat.type .. ", not a directory", false
